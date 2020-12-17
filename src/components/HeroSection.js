8de2073b76ab1fa9291dfe844b5ab6f3394d4065
Tushar <kansaltushar18@gmail.com>
2020-12-16 22:32:46 -0500

    Completed Website3

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css'
 function HeroSection() {
     return (
         <div className="hero-container">
-            <video src="videos/video2.mp4" autoPlay loop muted />
+            {/* <video src="videos/video2.mp4" autoPlay loop muted /> */}
             <h1>Hello! To My Website</h1>
             <p>Head over to Projects Section to know about me?</p>
             <div className="hero-btns">
